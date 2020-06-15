ac1c24ee3bf31504788beee1c3adcff7b549ee15
Jorrit Salverda <jsalverda@travix.com>
2020-06-15 15:50:39 +0200

    5 second delay for sse

--- GET-sse.js
+++ GET-sse.js
@@ -1,4 +1,4 @@
 const { delay, type, file } = require('connect-api-mocker/helpers')
 const path = require('path')
 
-module.exports = [delay(500), type('text/event-stream'), file(path.join(__dirname, './index.json'))]
+module.exports = [delay(5000), type('text/event-stream'), file(path.join(__dirname, './index.json'))]
